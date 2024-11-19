9b3585e4cfbc478655421308ed07204bb9497d9e
shelcia <shelcia1999@gmail.com>
2024-11-19 01:34:52 -0600

    🐛 matrix questions fix

--- pages/components/visualizations/matrix.R
+++ pages/components/visualizations/matrix.R
@@ -1,28 +1,35 @@
+library(dplyr)
+library(tidyr)
+library(plotly)
+library(stringr)
+
 matrix_questions <- function(example_matrix, demographic_variable, q_type) {
-  print(example_matrix)
+  # Debugging: Print inputs
   print(demographic_variable)
   print(q_type)
 
-
+  # Rename the second column to 'response'
   names(example_matrix)[2] <- "response"
 
-  # Since the questions are in one column, we need to separate them
-  # and then the question and response variables. ONLY FOR CCS QUESTIONS.
-  # In the allquestions_types questions in the CCS column = 1
+  # Separate 'response' into 'question' and 'answer'
   example_matrix <- example_matrix %>%
     separate_rows(response, sep = "; ") %>%
     separate(response, into = c("question", "answer"), sep = " - ")
+
+  # Convert 'answer' to a factor
   example_matrix$answer <- as.factor(example_matrix$answer)
 
+  example_matrix$question <- str_wrap(example_matrix$question, width = 40)
+
+  # Summarize data with .groups = "drop" to suppress warning
   matrix_summary <- example_matrix %>%
     group_by(!!sym(demographic_variable), question, answer) %>%
-    summarise(n = n()) %>%
+    summarise(n = n(), .groups = "drop") %>%
     mutate(freq = n / sum(n))
 
+  # Filter out NA demographics
   matrix_summary <- matrix_summary %>%
     filter(!is.na(!!sym(demographic_variable)))
-
-  # print(matrix_summary)
 
   # Define color sets based on q_type
   color_sets <- list(
@@ -38,78 +45,49 @@ matrix_questions <- function(example_matrix, demographic_variable, q_type) {
     importance = c(
       "#E66101", "#FDB863", "#FAE9D6", "#F7F7F7",
       "#B2ABD2", "#6753D7", "#3F009E"
-    )
+    ),
+    change = c("#1B9E77", "#D95F02", "#7570B3"), # Added 'change'
+    "agree1" = c("#E66101", "#FDB863", "#F7F7F7", "#B2ABD2", "#5E3C99"),
+    "agree2" = c("#E66101", "#FDB863", "#F7F7F7", "#B2ABD2", "#5E3C99")
+    # Add more as needed
   )
 
-  color_set <- color_sets[[q_type]]
+  # Clean q_type by removing spaces to match color_sets keys
+  q_type_clean <- gsub(" ", "", q_type)
+  color_set <- color_sets[[q_type_clean]]
 
-  matrix_summary <- merge(matrix_summary,
-    data.frame(answer = levels(matrix_summary$answer), col = color_set),
-    by = "answer"
-  )
-
-  # Create high and lows tables
-  highs <- matrix_summary %>%
-    filter(answer %in% c(
-      "Always", "Often", "Agree Strongly",
-      "Agree", "Strongly agree"
+  # Handle missing color_set
+  if (is.null(color_set)) {
+    warning(paste(
+      "Color set for q_type:", q_type,
+      "is not defined. Using default colors."
     ))
+    color_set <- RColorBrewer::brewer.pal(5, "PuOr") # default palette
+  }
 
-  lows <- matrix_summary %>%
-    filter(answer %in% c(
-      "Never", "Rarely", "Disagree Somewhat",
-      "Disagree", "Strongly disagree"
-    ))
+  # Ensure there are enough colors for all unique answers
+  unique_answers <- levels(matrix_summary$answer)
+  if (length(color_set) < length(unique_answers)) {
+    color_set <- colorRampPalette(color_set)(length(unique_answers))
+  }
 
-  mylevels <- levels(matrix_summary$answer)
+  # Create a named vector for colors
+  answer_colors <- setNames(color_set, unique_answers)
 
-  matrix_summary <- matrix_summary[, -ncol(matrix_summary)]
-
-  # print(matrix_summary)
-
-  switch(demographic_variable,
-    "Gender" = {
-      color_var <- ~Gender
-      legendgroup <- ~Gender
-      legendtext <- "Gender"
-    },
-    "income_recode" = {
-      color_var <- ~income_recode
-      legendgroup <- ~income_recode
-      legendtext <- "Income Groups"
-    },
-    "edu_recode" = {
-      color_var <- ~edu_recode
-      legendgroup <- ~edu_recode
-      legendtext <- "Education Levels"
-    },
-    "Year.of.Birth" = {
-      color_var <- ~Year.of.Birth
-      legendgroup <- ~Year.of.Birth
-      legendtext <- "Age Group"
-    },
-    "race_recode" = {
-      color_var <- ~race_recode
-      legendgroup <- ~race_recode
-      legendtext <- "Race"
-    }
-  )
-
+  # Create the plot
   matrix_visualization <- plot_ly(
     data = matrix_summary,
     x = ~freq,
     y = ~question,
-    text = ~ paste(
-      "<b>Response</b>: ", response
-    ),
+    text = ~ paste("<b>Response</b>: ", answer),
     hoverinfo = "text",
     type = "bar",
     orientation = "h",
-    color = color_var,
+    color = ~answer, # Color based on 'answer'
+    colors = answer_colors, # Assign specific colors
     width = 800,
     height = 400,
-    legendgroup = ~answer,
-    group = color_var
+    showlegend = TRUE
   ) %>%
     layout(
       xaxis = list(title = "Percentage of Respondents", family = "'Inter'"),
@@ -121,7 +99,7 @@ matrix_questions <- function(example_matrix, demographic_variable, q_type) {
         orientation = "h",
         font = list(size = 12, family = "'Inter'"),
         title = list(
-          text = legendtext,
+          text = "Responses",
           font = list(size = 16, family = "'Inter'")
         ),
         itemsizing = "constant",
